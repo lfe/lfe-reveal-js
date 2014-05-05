64e68c6d299a6d341d65fbd88f5ea7e757ed9852
Duncan McGreggor <duncan@cogitat.io>
2014-05-05 00:24:37 -0700

    Updated keywords.

--- package.exs
+++ package.exs
@@ -2,6 +2,7 @@ Expm.Package.new(
   name: "lfe-reveal-js",
   description: "An LFE/Exemplar Wrapper for Reveal.js",
   version: "0.0.1",
-  keywords: ["LFE", "Lisp", "Library", "HTML", "JavaScript", "JS"],
+  keywords: ["LFE", "Lisp", "Library", "YAWS", "HTTP", "HTML",
+             "JavaScript", "JS"],
   maintainers: [[name: "Duncan McGreggor", email: "oubiwann@cogitat.io"]],
   repositories: [[github: "lfe/lfe-reveal-js]])
